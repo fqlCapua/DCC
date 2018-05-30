d2eb254eb53bbbc419c3e57c330cba7550567cd1
朱雨 <15993055572@163.com@8c62a024-e827-49d0-81a4-5fd0b42e4ae5>
2018-05-30 14:03:50 +0000

    轉出至錢包 綁接口

--- src/page/roll_out.vue
+++ src/page/roll_out.vue
@@ -1,189 +1,155 @@
 <template>
-  <div class="rollOut">
-    <div class="form">
-      <label class="label" :for="index" v-for="(item, index) in form" :key="index">
-        <span class="name">{{ item.name }}</span>
-        <input class="input" :id="index" :type="item.type || 'text'" v-if="index !== 'newsCode'&& index !== 'phone' && index !== 'dccNum' " :placeholder="item.placeholder"  v-model="item.num" :readonly="item.readOnly">
-        <input class="input" :id="index" :type="item.type || 'text'" v-if="index == 'phone'" :placeholder="item.placeholder"  v-model="showPhone" :readonly="item.readOnly">
-        <button class="input_button" v-if="index === 'newsCode'" @click="getCode">{{ codeTime === 61 ? '獲取驗證碼' : codeTime + 's后重試' }}</button>
-        <input class="input_code" :id="index" :type="item.type || 'text'" v-if="index === 'newsCode'" v-model="item.num" :placeholder="item.placeholder">
-        <input class="input_code" :id="index" :type="item.type || 'text'" v-if="index === 'dccNum'" v-model="item.num" :placeholder="item.placeholder">
-        <span class="dcc" v-if="index === 'dccNum'" >DCC</span>
-      </label>
+  <div class="declaration">
+    <div action="" class="declaration-form">
+      <label for="" class="amount"><div>擁有DCC</div><input type="text" name=""v-model="zNum" readonly value="" placeholder="DCC數量"/></label>
+      <label for="" class="amount"><div>轉出数量</div><input type="text" name=""v-model="zcNum" value="" placeholder="請輸入轉出數量"/><span>DCC</span></label>
+      <label for=""><div>钱包地址</div><input type="text" name=""  value="" v-model="address" placeholder="請輸入錢包地址"/></label>
+      <label for="" class="code"><div>验证码</div><input type="text" name=""  value="" v-model="code"  placeholder="請輸入手機驗證碼"/><span @click="getCode">{{ codeTime === 61 ? '获取验证码' : `${codeTime}s后重试`}}</span></label>
     </div>
-    <button class="submit" @click="submit">確定</button>
+    <div class="submit" @click="submit">提交</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'rollOut',
-  data () {
-    return {
-      form: {
-        DCC: {
-          name: '擁有DCC',
-          placeholder: '請輸入數量',
-          num: '5252.255',
-          type: 'number',
-          readOnly: true
-        },
-        dccNum: {
-          name: '轉出數量',
-          placeholder: '請輸入轉出數量',
-          type: 'number',
-          num: ''
-        },
-         wallet: {
-          name: '錢包地址',
-          placeholder: '請輸入錢包地址',
-          num: ''
-        },
-        phone: {
-          name: '手機號碼',
-          placeholder: '我的手機號',
-          num: '18888888888',
-          readOnly: true
-        },
-        newsCode: {
-          name: '短信驗證碼',
-          placeholder: '請輸入驗證碼',
-          num: ''
-        }
-      },
-      codeTime: 61
-    }
-  },
-  mounted () {
-  	  let $that = this
-    this.$bus.$emit('pageHead',{
-    	title:"轉出至錢包",
-    	rightShow: true,
-    	rightText: '轉出記錄',
-    	rightBack:function(){
-    		  $that.$router.push('walletList')
-    	}
-    }),
-    this.init()
-  },
-  destroyed () {
-    this.$bus.$emit('pageHead')
-  },
-  computed: {
-    showPhone () {
-      let phoneStr
-      if (typeof (this.form.phone.num) !== 'string') {
-        phoneStr = this.form.phone.num.toString()
-      } else {
-        phoneStr = this.form.phone.num
+  export default {
+    name: 'personal',
+    data () {
+      return {
+        phone:localStorage.getItem("phone"),
+        code: '',
+        codeTime: 61,
+        address:'',
+        zNum:'',
+        zcNum:""
       }
-      return phoneStr.replace(/(\d{3})\d{4}(\d{4})/, '$1****$2')
-    }
-  },
-  methods: {
-    init () {
-      this.axios.post('quotation/decl_from_page').then(({data}) => {
-        this.form.phone.num = data.data.mobile
-        //      this.form.exchange.num = (this.form.usdt.num * data.data.rmb_dcc * data.data.usdt_rmb) + ' DCC'
-      })
     },
-    getCode () {
-      if (this.codeTime !== 61) return false
-      if (this.form.phone.num === '') return this.$bus.$emit('alert', '請輸入手機號碼')
-      this.codeTime = 60
-      let timer = setInterval(() => {
-        if (--this.codeTime === 0) {
-          clearInterval(timer)
-          this.codeTime = 61
+    mounted () {
+      this.$bus.$emit('pageHead', '轉出至錢包');
+      this.num()
+    },
+    destroyed () {
+      this.$bus.$emit('pageHead');
+    },
+    methods: {
+
+      num(){
+        this.axios.post('userHomePage', {
+          token:localStorage.getItem("token")
+        }).then(({data}) => {
+         this.zNum = data.data.DCC_total
+        })
+      },
+      getCode(){
+        if (this.codeTime !== 61) return false;
+        this.codeTime = 60
+        let timer = setInterval(() => {
+          if (--this.codeTime === 0) {
+            this.codeTime = 61
+            clearInterval(timer)
+          }
+        }, 1000)
+        this.axios.post('sms', {
+          phoneNo:this.phone
+        }).then(({data}) => {
+        })
+      },
+      submit(){
+        console.log(this.zNum)
+        console.log(this.zcNum)
+        if( this.zNum < this.zcNum ){
+          this.$bus.$emit('alert', '数量不能大于可用dcc的数值')
+          return false;
+        }else if((/0x[0-9a-zA-Z]{40}/.test(this.address))){
+          this.axios.post('withDrawDcc', {
+            token:localStorage.getItem("token"),
+//        钱包地址
+            wallet_address:this.address,
+//        数量
+            amount:this.zcNum,
+            sn:this.code
+          }).then(({data}) => {
+            console.log(data)
+            this.$bus.$emit('alertCer', data.data.msg)
+          })
+        }else{
+          this.$bus.$emit('alert', '钱包地址必须为0x开头的42位英文或者数字')
         }
-      }, 1000)
-      this.axios.post('Sms/send', {
-        type: 3,
-        phone: '18337131078'
-        //      phone: "this.form.phone.num"
-      }).then(({data}) => {
-        if (data.status !== 200) this.$bus.$emit('alert', data.message)
-      })
+
+
+// 			this.$router.push({path:'intoDetails' })
+      }
     },
-    submit () {
-      if (this.form.dccNum.num === '') return this.$bus.$emit('alert', '請輸入轉出數量')
-      if (this.form.wallet.num === '') return this.$bus.$emit('alert', '請輸入錢包地址')
-      if (this.form.newsCode.num === '') return this.$bus.$emit('alert', '請輸入驗證碼')
-      
-      this.axios.post('quotation/decl_from', {
-        usdt: this.form.usdt.num,
-        carry_usdt: this.form.backCode.num,
-        code: this.form.newsCode.num,
-        type: 1
-      }).then(({data}) => {
-        this.$bus.$emit('alert', data.message)
-        if (data.status === 200) this.$router.go(-1)
-      })
-      
+    components: {
+
     }
   }
-}
 </script>
 
-<style lang="scss">
-  @import '../assets/scss/style.scss';
-  .rollOut {
-    .head {
-      @include headBlack;
-    }
+<style lang="scss" scoped>
+  .declaration{
+
     padding-top:100px;
-    .form {
-      .label {
-        height: 100px;
-        display: block;
-        margin-bottom:22px;
-        background:#3f3c3c;
-        padding:0 42px;
-        box-sizing:border-box;
-        @include clearFloat;
-        .name {
-          float: left;
-          color: #d1cece;
-          font-size: 28px;
-          display: block;
-          height: 100%;
-          line-height: 100px;
-        }
-        .dcc{
-        	float: right;
-        	line-height: 100px;
-        	font-size:28px;
-        	color: #fff;
-        }
-        .input, .input_code {
-          float: left;
-          background: none;
-          color: #fff;
-          /*text-align: right;*/
-          padding-right: 10px;
-          height: 100%;
-          line-height: 100px;
-          width: 60%;
-          padding-left:25px;
-          font-size: 28px;
-        }
-        .input_code{
-        	width: 40%;
-        }
-        .input_button {
-          float: right;
-          padding: 0 10px;
-          background: none;
-          color: #d7a72e;
-          font-size: 28px;
-          height: 100%;
-        }
-      }
-    }
-    .submit {
-      @include submitButton;
-      width: 80%;
-      color: #111111;
-    }
+  .declaration-form{
+    margin-top:12px;
+
+    height:auto;
+    margin-bottom:335px;
+  label{
+    height:93px;
+    width: 100%;
+    line-height: 93px;
+    margin-top:10px;
+    padding-left: 18px;
+    display: flex;
+    background: #3f3c3c;
+  //justify-content: space-between;
+  div{
+    font-size: 28px;
+    color:#fff;
+    width:176px;
+  }
+  input{
+    font-size: 26px;
+     background: none;
+    color:#fff;
+    width: 70%;
+
+  }
+  }
+  .amount{
+    height:108px;
+    line-height: 108px;
+  input{
+    width:30%;
+  }
+  span{
+    color:#fff;
+    position:absolute;
+    right:40px;
+    font-size: 27px;
+  }
+  }
+  .code{
+  input{
+    width:50%;
+  }
+  span{
+    color:#ff9800;
+    font-size: 26px;
+    text-align: right;
+  }
+  }
+  }
+  .submit{
+    width:590px;
+    height:87px;
+    line-height:87px;
+    background: #2d8afd;
+    color: #fff;
+    font-size: 34px;
+    text-align: center;
+    margin:0 auto;
+    border-radius: 10px;
+  }
   }
 </style>

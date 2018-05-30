07e0d179b14e252e510e11b9d639f69109b412f9
朱雨 <15993055572@163.com@8c62a024-e827-49d0-81a4-5fd0b42e4ae5>
2018-05-30 06:37:13 +0000

    no commit message

--- src/page/login.vue
+++ src/page/login.vue
@@ -14,9 +14,15 @@
       <span class="icon iconfont icon-mima54"></span>
       <input class="input" id="pass" type="password" v-model="pass" placeholder="請輸入登錄密碼">
     </label>
+    <label class="label" for="yzm">
+     <span class="icon iconfont icon-theearth2diqiu"></span>
+      <input class="input" id="yzm" type="password" v-model="captCha" placeholder="请输入验证码">
+      <span class="yzmBox" @click="tab"><img :src="img" alt="" width="100%" height="100%"></span>
+
+    </label>
     <div class="option">
-      <!-- <router-link to="" tag="span" class="route">忘記密碼?</router-link>
-      <router-link to="register" tag="span" class="route">快速注冊</router-link> -->
+      <router-link to="" tag="span" class="route">忘記密碼?</router-link>
+      <router-link to="register" tag="span" class="route">快速注冊</router-link>
     </div>
     <button class="submit" @click="submit">登錄</button>
   </div>
@@ -28,26 +34,116 @@ export default {
   data () {
     return {
       phone: '',
-      pass: ''
+      pass: '',
+      show:0,
+      captCha:'',
+      img:'',
     }
   },
+  mounted () {
+    this.imgCaptcha();
+    this.getCookie();
+  },
   methods: {
+
     submit () {
-      if (this.phone === '') return this.$bus.$emit('alert', '請輸入手機號碼')
-      if (this.pass === '') return this.$bus.$emit('alert', '請輸入登錄密碼')
+      if (this.phone === '') return this.$bus.$emit('alert', '请输入手机号');
+      if (this.pass === '') return this.$bus.$emit('alert', '请输入登录密码');
+      //保存的账号
+      let name=this.phone;
+      //保存的密码
+      let pass=this.pass;
+      if(this.show===0){
+        //传入账号名，密码，和保存天数3个参数
+//        this.setCookie(name,pass,7);
+      }
       this.doLogin()
     },
-    doLogin () {
-      this.axios.post('/Auth/login', {
-        phone: this.phone,
-        password: this.pass
+    say(){
+      this.show = !this.show;
+    },
+
+    doLogin() {
+      let $that=this;
+      this.axios.post('userLogin', {
+        username: this.phone,
+        password: this.pass,
+        captcha:this.captCha,
+        cap:this.cap
       }).then(({data}) => {
-        if (data.status === 200) {
-          this.$router.push('/')
-        } else {
-          this.$bus.$emit('alert', data.message)
+        if (data.ret === 0){
+          let token=data.data.token;
+          window.localStorage.setItem('token',token);
+          this.$bus.$emit('alertCer', {
+            msg:"登录成功"
+          });
+          setTimeout(function () {
+            $that.$router.push('index');
+          },2000)
         }
+        if(data.msg === '图片验证码不匹配'){
+          this.imgCaptcha();
+          $that.captCha =''
+        }
+        if(data.msg === '用户名和密码不匹配'){
+          $that.pass =''
+        }
+        if(data.ret === 1){
+          //this.$bus.$emit('alertCer', data.msg);
+          this.$bus.$emit('alertCer', {
+            msg:data.msg
+          });
+
+        }
+
       })
+    },
+    //校验图片验证
+    imgCaptcha(){
+      this.axios.post('captchaInfo', {
+      }).then(({data}) =>{
+        this.img= data.data.img;
+        this.cap= data.data.str;
+      })
+    },
+    checkoutCaptcha () {
+      let $that=this;
+      this.axios.post('checkCaptcha', {
+        cap: $that.cap,
+        captcha:$that.captCha
+      }).then(({data}) => {
+        console.log(data)
+      })
+    },
+    tab(){
+      this.imgCaptcha();
+    },
+    //设置cookie
+    setCookie(c_name,c_pwd,exdays) {
+      var exdate=new Date();//获取时间
+      exdate.setTime(exdate.getTime() + 24*60*60*1000*exdays);//保存的天数
+      //字符串拼接cookie
+      window.document.cookie="userName"+ "=" +c_name+";path=/;expires="+exdate.toGMTString();
+      window.document.cookie="userPwd"+"="+c_pwd+";path=/;expires="+exdate.toGMTString();
+    },
+    //读取cookie
+    getCookie:function () {
+      if (document.cookie.length>0) {
+        var arr=document.cookie.split('; ');//这里显示的格式需要切割一下自己可输出看下
+        for(var i=0;i<arr.length;i++){
+          var arr2=arr[i].split('=');//再次切割
+          //判断查找相对应的值
+          if(arr2[0]=='userName'){
+            this.phone=arr2[1];//保存到保存数据的地方
+          }else if(arr2[0]=='userPwd'){
+            this.pass=arr2[1];
+          }
+        }
+      }
+    },
+    //清除cookie
+    clearCookie:function () {
+      this.setCookie("","",-1);//修改2值都为空，天数为负1天就好了
     }
   }
 }
@@ -104,6 +200,14 @@ export default {
         color: $mainColor;
         height: 85px;
         background: none;
+      }
+      .yzmBox{
+      	float: right;
+      	display: inline-block;
+      	width: 180px;
+      	height: 60px;
+      	margin-bottom: 10px;
+      	border: 1px solid #fff;
       }
     }
     .option {

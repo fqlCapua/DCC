3a7830ba7dee09508a2963b3052afe054b1b3da3
刘佳 <993654866@qq.com>
2018-05-30 06:26:46 +0000

    no commit message

--- src/main.js
+++ src/main.js
@@ -6,39 +6,32 @@ import router from './router'
 import axios from 'axios'
 import Qs from 'qs'
 
-Vue.config.productionTip = false;
-
-Vue.prototype.$bus = new Vue();
-// Vue.prototype.$bus.baseURL = 'http://121.42.251.110:18888';
-
+Vue.config.productionTip = false
+Vue.prototype.$bus = new Vue()
+let protocolStr = document.location.protocol
+console.log(protocolStr)
 Vue.prototype.axios = axios.create({
-  baseURL: 'http://api.zhongguozhaishi.com/captchaInfo',
+  baseURL: protocolStr=== 'https:'? 'https://api.zhongguozhaishi.com/': 'http://api.zhongguozhaishi.com/',
   timeout: 10000,
   withCredentials: false,
   // 这时候我们通过Qs.stringify转换为表单查询参数
   transformRequest: [function (data) {
-    data = Qs.stringify(data);
+     data  =  JSON.stringify(data)
     return data
   }],
+
   // 设置Content-Type
-  headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
 })
-
-Vue.prototype.axios.interceptors.response.use(res => {
-  if (res.data.status === 401) {
-    router.push('login');
-    Vue.prototype.$bus.$emit('alert', '請登錄')
-  }
-  if(localStorage.getItem('token') ===""){
-    router.push('login');
-    Vue.prototype.$bus.$emit('alert', '請登錄')
-  }
-  return res
-}, err => {
-  console.log(err)
-}
-
-)
+//Vue.prototype.axios.interceptors.response.use(res => {
+//if (res.data.ret === 401) {
+//  router.push('login')
+//  Vue.prototype.$bus.$emit('alert', '请重新登录')
+//}
+//return res
+//}, err => {
+//console.log(err)
+//})
 
 /* eslint-disable no-new */
 new Vue({

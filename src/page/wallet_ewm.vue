10a420a92dfc3d0d218c2753fe1d0db2b5df7c52
李广宇 <935194848@qq.com@8c62a024-e827-49d0-81a4-5fd0b42e4ae5>
2018-05-31 07:37:24 +0000

    no commit message

--- src/page/wallet_ewm.vue
+++ src/page/wallet_ewm.vue
@@ -2,7 +2,7 @@
 	<div class="walletEwm">
 		<!-- 頭部 -->
 		<div class="head">
-			<router-link to="">
+			<router-link to="userCenter">
 				<span class="icon iconfont icon-fanhuijiantou returnBtn"></span>
 			</router-link>
 			<h2 class="title">钱包二维码</h2>
@@ -14,9 +14,9 @@
 			<p class="name">{{item.name}}</p>
 			<p class="number">{{item.number}}</p>
 		</div>
-		<img src="" alt="" />
+		<img :src="item.ewmImg" alt="" />
 		
-		<p class="saveImg">保存为图片</p>
+		<p class="saveImg">保存為圖片</p>
 	</div>
 		
 	</div>
@@ -28,7 +28,7 @@
 		data(){
 			return{
 				ewmInfo:[
-				{name:'刘洋',number:'415asdsad56465asd',ewmImg:'###'}
+				{name:'劉洋',number:'415asdsad56465asd',ewmImg:require('../assets/images/wallet_ewm.png')}
 				
 				]
 				
@@ -62,6 +62,34 @@
 			background: #fff;
 			width: 674px;
 			height: 874px;
+			margin:180px auto;
+			border-radius: 10px;
+			.infoBox{
+				padding: 65px 0;
+				text-align: center;
+				p{
+					margin-bottom: 20px;
+					font-size: 28px;
+				}
+				p:first-child{
+					font-weight: bold;
+				}
+				P:last-child{
+					margin-bottom: 0;
+				}
+			}
+			img{
+				display: block;
+				width: 496px;
+				height: 496px;
+				margin: 0 auto;
+			}
+			.saveImg{
+				text-align: center;
+				margin: 60px 0;
+				font-size: 26px;
+				color: rgb(215,168,43);
+			}
 		}
 		
 		

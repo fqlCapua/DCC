b733dcf5b7f61fed9e39e8f4c3b9b17dda4ec4d7
朱雨 <15993055572@163.com>
2018-06-01 08:08:37 +0000

    no commit message

--- src/page/market.vue
+++ src/page/market.vue
@@ -62,9 +62,26 @@ export default {
       this.axios.post('rewardIndex',{
       	token:this.getCookie("token")
       }).then(({data}) => {
-        this.allMoney = data.data.total
+        this.allMoney = this.formatNum(data.data.total,4)
       })
-    }
+    },
+    // 数字格式化
+    formatNum (s, n) {
+      /*
+       * 参数说明：
+       * s：要格式化的数字
+       * n：保留几位小数
+       * */
+      n = n > 0 && n <= 20 ? n : 2
+      s = parseFloat((s + '').replace(/[^\d.-]/g, '')).toFixed(n) + ''
+      let l = s.split('.')[0].split('').reverse()
+      let r = s.split('.')[1]
+      let t = ''
+      for (let i = 0; i < l.length; i++) {
+        t += l[i] + ((i + 1) % 3 === 0 && (i + 1) !== l.length ? ',' : '')
+      }
+      return t.split('').reverse().join('') + '.' + r
+    },
   }
 }
 </script>

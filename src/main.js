c2aa84f912d557f990ca74e8c6cd77aa12167d47
朱雨 <15993055572@163.com>
2018-06-01 02:24:54 +0000

    no commit message

--- src/main.js
+++ src/main.js
@@ -19,35 +19,13 @@ Vue.prototype.axios = axios.create({
      data  =  JSON.stringify(data)
     return data
   }],
-
   // 设置Content-Type
 headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
 })
-//Vue.prototype.axios.interceptors.response.use(res => {
-//if (res.data.ret === 401) {
-//  router.push('login')
-//  Vue.prototype.$bus.$emit('alert', '请重新登录')
-//}
-//return res
-//}, err => {
-//console.log(err)
-//})
 
- 
-// Vue.prototype.axios.interceptors.response.use(res => {
-//   if (res.data.status === 2000) {
-//     router.push('login')
-//     Vue.prototype.$bus.$emit('alert', '請登錄')
-//   }
-//   return res
-// }, err => {
-//   console.log(err)
-// })
-
- 
 Vue.prototype.axios.interceptors.response.use(res => {
-  if (res.data.status === 2000) {
-    router.push('login')
+  if (res.data.status === 401) {
+    this.$router.push('/login')
     Vue.prototype.$bus.$emit('alert', '請登錄')
   }
   return res
@@ -55,7 +33,31 @@ Vue.prototype.axios.interceptors.response.use(res => {
   console.log(err)
 })
 
- 
+//设置cookie
+Vue.prototype.setCookie = function(c_name,value,expiredays) {
+  var exdate=new Date()
+  exdate.setDate(exdate.getDate()+expiredays)
+  document.cookie=c_name+ "=" +escape(value)+
+    ((expiredays==null) ? "" : ";expires="+exdate.toGMTString())
+};
+
+
+//获取cookie
+Vue.prototype.getCookie=function(c_name) {
+  if (document.cookie.length>0)
+  {
+    var  c_start=document.cookie.indexOf(c_name + "=")
+    if (c_start!=-1)
+    {
+      c_start=c_start + c_name.length+1
+      var c_end=document.cookie.indexOf(";",c_start)
+      if (c_end==-1) c_end=document.cookie.length
+      return unescape(document.cookie.substring(c_start,c_end))
+    }
+  }
+  return ""
+};
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',

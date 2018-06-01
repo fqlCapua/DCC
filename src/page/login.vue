b6f3e17914d690662027929e40267ccab2ec2458
朱雨 <15993055572@163.com@8c62a024-e827-49d0-81a4-5fd0b42e4ae5>
2018-06-01 02:23:29 +0000

    no commit message

--- src/page/login.vue
+++ src/page/login.vue
@@ -63,7 +63,8 @@ export default {
       }).then(({data}) => {
         if (data.ret === 0){
           let token=data.data.token;
-          window.localStorage.setItem('token',token);
+//          window.localStorage.setItem('token',token);
+            this.setCookie('token',token);
           this.$bus.$emit('alertCer', {
             msg:"登录成功"
           });

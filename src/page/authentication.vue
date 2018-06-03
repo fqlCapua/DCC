2aeca08744b2f5007e22e1726045bad28ced70ff
朱雨 <15993055572@163.com@8c62a024-e827-49d0-81a4-5fd0b42e4ae5>
2018-06-03 03:52:36 +0000

    no commit message

---
+++ src/page/authentication.vue
@@ -0,0 +1,61 @@
+<template>
+	<div class="authentication">
+		<div class="successPrompt">
+			<img src="../assets/images/ordeInfo_bg.png" />
+			<p class="successP">認證成功</p>
+		</div>
+		<!--返回首页-->
+		<router-link to="index">
+			<p class="returnBtn">返回首頁</p>
+		</router-link>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'authentication',
+		data() {
+			return {
+
+			}
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	@import '../assets/scss/style.scss';
+	.authentication {
+		padding-top: 80px;
+		.successPrompt {
+			height: 522px;
+			img {
+				display: block;
+				width: 285px;
+				margin: 0 auto;
+			}
+			.successP {
+				color: #fff;
+				margin: 20px 0 58px;
+				font-size: 32px;
+			}
+			.nxddP {
+				color: rgb(102, 102, 102);
+				font-size: 30px;
+			}
+			p {
+				text-align: center;
+			}
+		}
+		.returnBtn {
+			width: 600px;
+			height: 90px;
+			color: #fff;
+			font-size: 28px;
+			border-radius: 50px;
+			text-align: center;
+			line-height: 90px;
+			margin: 320px auto;
+			background: rgb(215,168,43);
+		}
+	}
+</style>

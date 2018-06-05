fe472f4c0d85c863fd252d81214e304540470b69
fqlCapua <13523450460@sina.cn@8c62a024-e827-49d0-81a4-5fd0b42e4ae5>
2018-06-05 08:29:49 +0000

    capua 二维码

--- src/page/accounts.vue
+++ src/page/accounts.vue
@@ -3,7 +3,7 @@
     <div action="" class="declaration-form">
       <label for="" class="amount"><div>擁有DCC</div><input type="text" name=""v-model="zNum" readonly value="" placeholder="DCC數量"/></label>
       <label for="" class="amount"><div>轉出数量</div><input type="text" name=""v-model="zcNum" value="" placeholder="請輸入轉出數量"/><span>DCC</span></label>
-      <label for=""><div>對方帳號</div><input type="text"  name=""  value="" v-model="address" placeholder="請輸入對方帳號/手機號"/></label>
+      <label for=""><div>對方帳號</div><input type="text"  name=""  value="" v-model="address" placeholder="請輸入對方帳號/手機號"/> <span class=" iconfont icon-richscan_icon"></span></label>
       <label for="" class="code"><div>验证码</div><input type="text" name=""  value="" v-model="code"  placeholder="請輸入手機驗證碼"/><span @click="getCode">{{ codeTime == 61 ? '获取验证码' : codeTime+'s后重试'}}</span></label>
     </div>
     <div class="submit" @click="submit">提交</div>
@@ -178,5 +178,10 @@
     border-radius:70px 70px;
   }
 
+  }
+  .icon-richscan_icon{
+    font-size:2em;
+    padding-right:40px;
+    color:$mainColor;
   }
 </style>

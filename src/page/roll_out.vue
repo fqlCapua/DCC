7ea5387a76493566c843f30250acb7cd93ed02fd
刘佳 <993654866@qq.com@8c62a024-e827-49d0-81a4-5fd0b42e4ae5>
2018-06-06 01:33:06 +0000

    no commit message

--- src/page/roll_out.vue
+++ src/page/roll_out.vue
@@ -4,7 +4,7 @@
       <label for="" class="amount"><div>擁有DCC</div><input type="text" name=""v-model="zNum" readonly value="" placeholder="DCC數量"/></label>
       <label for="" class="amount"><div>轉出数量</div><input type="text" name=""v-model="zcNum" value="" placeholder="請輸入轉出數量"/><span>DCC</span></label>
       <label for=""><div>钱包地址</div><input type="text" name=""  value="" v-model="address" placeholder="請輸入錢包地址"/></label>
-      <label for="" class="code"><div>验证码</div><input type="text" name=""  value="" v-model="code"  placeholder="請輸入手機驗證碼"/><span @click="getCode">{{ codeTime === 61 ? '获取验证码' : codeTime+'s后重试'}}</span></label>
+      <label for="" class="code"><div>验证码</div><input type="text" name=""  value="" v-model="code"  placeholder="請輸入手機驗證碼"/><span @click="getCode">{{ codeTime == 61 ? '获取验证码' : codeTime+'s后重试'}}</span></label>
     </div>
     <div class="submit" @click="submit">提交</div>
   </div>
@@ -15,7 +15,7 @@
     name: 'personal',
     data () {
       return {
-        phone:localStorage.getItem("phone"),
+        phone:'',
         code: '',
         codeTime: 61,
         address:'',
@@ -24,6 +24,7 @@
       }
     },
     mounted () {
+    	this.phone = this.getCookie("phone");
       let $that =this;
       this.$bus.$emit('pageHead',{
         title:"轉出至錢包",

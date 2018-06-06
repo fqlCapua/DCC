4180462dd4a1fe3d42a185c8cd185091a1dc61c9
朱雨 <15993055572@163.com@8c62a024-e827-49d0-81a4-5fd0b42e4ae5>
2018-06-06 08:22:20 +0000

    no commit message

--- src/page/index.vue
+++ src/page/index.vue
@@ -162,17 +162,16 @@ export default {
      * s：要格式化的数字
      * n：保留几位小数
      * */
-      n = n > 0 && n <= 20 ? n : 2
-      s = parseFloat((s + '').replace(/[^\d.-]/g, '')).toFixed(n) + ''
-      let l = s.split('.')[0].split('').reverse()
-      let r = s.split('.')[1]
-      let t = ''
+      n = n > 0 && n <= 20 ? n : 2;
+      s = parseFloat((s + '').replace(/[^\d.-]/g, '')).toFixed(n) + '';
+      let l = s.split('.')[0].split('').reverse();
+      let r = s.split('.')[1];
+      let t = '';
       for (let i = 0; i < l.length; i++) {
         t += l[i] + ((i + 1) % 3 === 0 && (i + 1) !== l.length ? ',' : '')
       }
       return t.split('').reverse().join('') + '.' + r
     },
-
   }
 }
 </script>

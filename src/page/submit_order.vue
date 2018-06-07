8fe0daf89b2252fe8de05d06e331f9117d2abaa5
刘佳 <993654866@qq.com>
2018-06-07 06:21:48 +0000

    no commit message

--- src/page/submit_order.vue
+++ src/page/submit_order.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="declaration">
+  <div class="submitOrder">
     <div action="" class="declaration-form">
       <label for="" class="amount"><div>售價USDT</div><input type="text" name=""v-model="ustd" readonly value="" placeholder="DCC數量"/><span>USDT</span></label>
-      <label for=""><div>USDT提款嗎</div><input type="text" name=""  value="" v-model="usdt_code" placeholder="請輸入USTD提款嗎"/></label>
-      <label for="" class="code"><div>验证码</div><input type="text" name=""  value="" v-model="code"  placeholder="請輸入手機驗證碼"/><span @click="getCode">{{ codeTime === 61 ? '获取验证码' : `${codeTime}s后重试`}}</span></label>
+      <label for="" class="code">
+      	<div>USDT提款碼</div>
+      	<input class="Usdt_code" type="text" name=""  value="" v-model="usdt_code" placeholder="請輸入USTD提款碼"/>
+      	<span class="iconr iconfont icon-richscan_icon">
+      		<input type="file" class="file12" @change="onFileChange">
+      	</span>
+      </label>
+      <label for="" class="code">
+      	<div>验证码</div>
+      	<input type="text" name=""  value="" v-model="code"  placeholder="請輸入手機驗證碼"/>
+      	<span @click="getCode">{{ codeTime === 61 ? '获取验证码' : `${codeTime}s后重试`}}</span>
+      </label>
     </div>
     <div class="submit" @click="submit">提交</div>
     <orderSuccess :content ="orderSuccess"></orderSuccess>
@@ -13,7 +23,7 @@
 <script>
 import orderSuccess from './orderSuccess'
   export default {
-    name: 'personal',
+    name: 'submitOrder',
     data () {
       return {
         phone:"",
@@ -99,16 +109,33 @@ import orderSuccess from './orderSuccess'
           this.mac=data.data;
           //console.log(this.mac[0].amount)
           if( this.getCookie('id') == 1){
-            this.ustd =this.mac[0].amount
+            this.ustd =this.formatNum(this.mac[0].amount,4) 
           }
           if( this.getCookie('id') == 2){
-            this.ustd =this.mac[1].amount
+            this.ustd =this.formatNum(this.mac[1].amount,4) 
           }
           if( this.getCookie('id') == 3){
-            this.ustd =this.mac[2].amount
+            this.ustd =this.formatNum(this.mac[2].amount,4) 
           }
         })
       },
+      //扫码二维码
+      onFileChange(e) {
+    	let  $that  =this
+      var files = e.target.files || e.dataTransfer.files;
+      if (!files.length)
+        return;
+//    this.createImage(files[0]);
+       var fileUrl = this.getObjectURL(files[0]);
+       qrcode.decode(fileUrl);
+       qrcode.callback = function(imgMsg){  
+				    if(imgMsg.indexOf("error") > -1){
+				    	return $that.$bus.$emit('alert', "二维码无效")
+				    }else {
+				       $that.usdt_code = imgMsg
+				    }
+				} 
+    },
       getCode(){
         if (this.codeTime !== 61) return false;
         this.codeTime = 60
@@ -162,7 +189,7 @@ import orderSuccess from './orderSuccess'
 
 <style lang="scss" scoped>
   @import '../assets/scss/style.scss';
-  .declaration{
+  .submitOrder{
     padding-top:100px;
   .declaration-form{
     margin-top:12px;
@@ -176,7 +203,7 @@ import orderSuccess from './orderSuccess'
     padding-left: 18px;
     display: flex;
     background: #3f3c3c;
-  //justify-content: space-between;
+   /*justify-content: space-around;*/
   div{
     font-size: 28px;
     color:#fff;
@@ -204,15 +231,34 @@ import orderSuccess from './orderSuccess'
   }
   }
   .code{
-  input{
-    width:50%;
+	  input{
+	    width:50%;
+	  }
+	  span{
+	    color:#D7A82B;
+	    font-size: 26px;
+	    text-align: right;
+	  }
+	  .Usdt_code{
+	  	width: 60%;
+	  }
+	  .iconr{
+	  	font-size: 40px;
+	  	display: block;
+	  	position: relative;
+	  	 .file12{
+				  outline: none;
+				  opacity: 0;
+				  width: 100%;
+				  height: 100%;
+				  position: absolute;
+				  top: 0;
+				  left:0;
+				  z-index: 10;
+		  }
+	  }
   }
-  span{
-    color:#D7A82B;
-    font-size: 26px;
-    text-align: right;
-  }
-  }
+
   }
   .submit{
     width:590px;
@@ -225,6 +271,6 @@ import orderSuccess from './orderSuccess'
     margin:0 auto;
     border-radius:70px 70px;
   }
-
+ 
   }
 </style>
